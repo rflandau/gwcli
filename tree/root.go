44e35abe225848dda81563585cdcbde6500f8760
R Landau <rflandau@pm.me>
2024-05-21 17:04:55 -0400

    Add --no-interactive flag and EnforceLogin global check (skeleton)

--- tree/root.go
+++ tree/root.go
@@ -8,47 +8,62 @@ import (
 	"gwcli/tree/search"
 	"gwcli/tree/systems"
 	"gwcli/tree/tools"
+	"gwcli/treeutils"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "gwcli",
-	Short: "Gravwell CLI Client",
-	Long: "gwcli is a CLI client for interacting with your Gravwell instance directly from your terminal.\n" +
-		"It can be used non-interactively in your scripts or interactively via the built-in TUI.\n" +
-		"To invoke the TUI, simply call `gwcli`.",
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+func EnforceLogin(cmd *cobra.Command, args []string) error {
+	// TODO check for token or supply user with login model
+	return nil
+}
+
+/** Generate Flags populates all root-relevant flags (ergo global and root-local flags) */
+func GenerateFlags(root *cobra.Command) {
+	root.PersistentFlags().Bool("no-interactive", false, "Disallows gwcli from entering interactive mode and prints context help instead.\nRecommended for use in scripts to avoid hanging on a malformed command")
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	// construct the tree
+	var rootCmd = &cobra.Command{
+		Use:   "gwcli",
+		Short: "Gravwell CLI Client",
+		Long: "gwcli is a CLI client for interacting with your Gravwell instance directly from your terminal.\n" +
+			"It can be used non-interactively in your scripts or interactively via the built-in TUI.\n" +
+			"To invoke the TUI, simply call `gwcli`.",
+		PersistentPreRunE: EnforceLogin,
+		Run:               treeutils.NavRun,
+	}
+
+	// associate flags
+	GenerateFlags(rootCmd)
+
+	// set up nav and action groups
+	rootCmd.AddGroup(&cobra.Group{ID: treeutils.NavID, Title: "Navigation"})
+	rootCmd.AddGroup(&cobra.Group{ID: treeutils.ActionID, Title: "Actions"})
+
+	// add root to nav group
+	rootCmd.GroupID = treeutils.NavID
 
 	// add direct descendents, which will each add their descendents
 	rootCmd.AddCommand(systems.GenerateTree())
 	rootCmd.AddCommand(search.GenerateTree())
 	rootCmd.AddCommand(tools.GenerateTree())
 
+	if !rootCmd.AllChildCommandsHaveGroup() {
+		// TODO move this into a testing package
+		panic("some children missing a group")
+	}
+
+	// configure Windows mouse trap
+	cobra.MousetrapHelpText = "This is a command line tool.\n" +
+		"You need to open cmd.exe and run it from there.\n" +
+		"Press Return to close.\n"
+	cobra.MousetrapDisplayDuration = 0
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.gwcli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
