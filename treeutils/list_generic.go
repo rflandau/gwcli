65824939a5113ce6ade0b1d902aa8b7825b0a1dc
R Landau <rflandau@pm.me>
2024-05-31 13:54:18 -0400

    Create StructFields to return the list of fully qualified field names in a struct through any depth

--- treeutils/list_generic.go
+++ treeutils/list_generic.go
@@ -6,6 +6,7 @@
 package treeutils
 
 import (
+	"errors"
 	"fmt"
 	"gwcli/clilog"
 	"gwcli/connection"
@@ -27,6 +28,14 @@ const (
 	csv
 	table
 )
+
+//#region errors
+
+const (
+	ErrNotAStruct string = "given value is not a struct or pointer to a struct"
+)
+
+//#endregion
 
 func (f format) String() string {
 	switch f {
@@ -124,7 +133,59 @@ func determineFormat(cmd *cobra.Command) format {
 }
 
 // Returns a list of all fields in the struct *definition*
-func StructFields(st any) (columns []string) {
+func StructFields(st any) (columns []string, err error) {
+	to := reflect.TypeOf(st)
+	if to.Kind() == reflect.Pointer { // dereference
+		to = to.Elem()
+	}
+	if to.Kind() != reflect.Struct { // prerequisite
+		return nil, errors.New(ErrNotAStruct)
+	}
+	numFields := to.NumField()
+	columns = []string{}
+
+	// for each field
+	//	if the field is not a struct, append it to the columns
+	//	if the field is a struct, repeat
+
+	for i := 0; i < numFields; i++ {
+		columns = append(columns, innerStructFields("", to.Field(i))...)
+	}
+
+	return columns, nil
+}
+
+// innerStructFields is a helper function for StructFields, returning the
+// qualified name of the given field or the list of qualified names of its
+// children, if a struct.
+// Operates recursively on the given field if it is a struct.
+// Operates down the struct, in field-order.
+func innerStructFields(qualification string, field reflect.StructField) []string {
+	var columns []string = []string{}
+	if field.Type.Kind() == reflect.Struct {
+		for k := 0; k < field.Type.NumField(); k++ {
+			var innerQual string
+			if qualification == "" {
+				innerQual = field.Name
+			} else {
+				innerQual = qualification + "." + field.Name
+			}
+
+			columns = append(columns, innerStructFields(innerQual, field.Type.Field(k))...)
+		}
+	} else {
+		if qualification == "" {
+			columns = append(columns, field.Name)
+		} else {
+			columns = append(columns, qualification+"."+field.Name)
+		}
+	}
+
+	return columns
+}
+
+// Returns a list of all fields in the struct *definition*
+func StructFieldsO(st any) (columns []string) {
 	types := reflect.ValueOf(st).Type()
 	numFields := types.NumField()
 	columns = make([]string, numFields)
@@ -132,7 +193,12 @@ func StructFields(st any) (columns []string) {
 	// TODO use FieldByIndex to dig into embedded types and the direct field names
 
 	for i := 0; i < numFields; i++ {
-		columns[i] = types.Field(i).Name
+		field := types.Field(i)
+		fbi := types.FieldByIndex(field.Index)
+		fmt.Printf("{%d}\nfield: %#v\nfieldIndex: %+v\nfbi: %+v", i, field, field.Index, fbi)
+		columns[i] = fbi.Name
+
+		//columns[i] = types.Field(i).Name
 	}
 
 	return
