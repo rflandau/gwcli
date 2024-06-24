799f9dfdc1eeee5c8db7088609b0a40e1d9cdfe2
R Landau <rflandau@pm.me>
2024-06-24 09:37:34 -0400

    implement datascope

--- datascope/datascope.go
+++ datascope/datascope.go
@@ -21,56 +21,111 @@ import (
 //#region For Cobra Usage
 
 type DataScope struct {
-	vp    viewport.Model
-	pager paginator.Model
-	ready bool
-	data  []string
-	done  bool
-	Title string
+	vp            viewport.Model
+	pager         paginator.Model
+	ready         bool
+	data          []string // complete set of data to be paged
+	done          bool     // user has used a kill key
+	Title         string   // displayed in the header box
+	motherRunning bool     // without Mother's support, we need to handle killkeys and death alone
+	margins       struct {
+		header    string
+		hdrHeight int
+		footer    string
+		ftrHeight int
+	}
 }
 
 func (s DataScope) Init() tea.Cmd {
+	// set header
+	s.margins.header = func() string {
+		title := viewportHeaderBoxStyle.Render(s.Title)
+		line := strings.Repeat("─", max(0, s.vp.Width-lipgloss.Width(title)))
+		dots := s.pager.View() + "\n"
+		paragraph := lipgloss.JoinVertical(lipgloss.Center, dots, line)
+		return lipgloss.JoinHorizontal(lipgloss.Center, title, paragraph)
+	}()
+
+	// set footer
+
+	s.margins.footer = func() string {
+		percent := infoStyle.Render(fmt.Sprintf("%3.f%%", s.vp.ScrollPercent()*100))
+		line := strings.Repeat("─", max(0, s.vp.Width-lipgloss.Width(percent)))
+
+		help := "h/l ←/→ page • q: quit\n"
+		paragraph := lipgloss.JoinVertical(lipgloss.Center, line, help)
+		return lipgloss.JoinHorizontal(lipgloss.Center, paragraph, percent)
+	}()
+
+	// pre-set heights
+	s.margins.hdrHeight = lipgloss.Height(s.margins.header)
+	s.margins.ftrHeight = lipgloss.Height(s.margins.footer)
+
 	return nil
 }
 
 func (s DataScope) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return s, nil
+	var (
+		cmd  tea.Cmd
+		cmds []tea.Cmd
+	)
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc", "ctrl+c":
+			if s.motherRunning {
+				s.done = true
+				return s, tea.ExitAltScreen
+			}
+			return s, tea.Batch(tea.ExitAltScreen, tea.Quit)
+		}
+	case tea.WindowSizeMsg:
+
+		marginHeight := s.margins.hdrHeight + s.margins.ftrHeight // extra space not showing content
+
+		if !s.ready { // if we are not ready, use these dimensions to become ready
+			s.vp = viewport.New(msg.Width, msg.Height-marginHeight)
+			s.vp.YPosition = s.margins.hdrHeight
+			s.vp.HighPerformanceRendering = false
+			s.vp.SetContent(s.displayPage())
+			s.ready = true
+			cmds = append(cmds, tea.EnterAltScreen) // start the alt buffer
+		} else { // just an update
+			s.vp.Width = msg.Width
+			s.vp.Height = msg.Height - marginHeight
+		}
+	}
+	s.pager, cmd = s.pager.Update(msg)
+	cmds = append(cmds, cmd)
+	// pass the new content to the view
+	s.vp.SetContent(s.displayPage())
+	s.vp, cmd = s.vp.Update(msg)
+	cmds = append(cmds, cmd)
+	return s, tea.Sequence(cmds...)
 }
 
 func (s DataScope) View() string {
-	return ""
-}
+	if s.Done() {
+		return "\nQuitting..."
+	}
+	if !s.ready {
+		return "\nInitializing..."
+	}
 
-// Generates a string representation of the top margin and header box
-func (s DataScope) header() string {
-	title := viewportHeaderBoxStyle.Render(s.Title)
-	line := strings.Repeat("─", max(0, s.vp.Width-lipgloss.Width(title)))
-	return lipgloss.JoinHorizontal(lipgloss.Center, title, line)
-}
-
-// Generates a string representation of the bottom margin and footer box
-func (s DataScope) footer() string {
-	info := infoStyle.Render(fmt.Sprintf("%3.f%%", s.vp.ScrollPercent()*100))
-	line := strings.Repeat("─", max(0, s.vp.Width-lipgloss.Width(info)))
-
-	upper := s.pager.View() + "\n"
-	lower := "h/l ←/→ page • q: quit\n"
-	builtLine := lipgloss.JoinVertical(lipgloss.Center, upper, line, lower)
-
-	return lipgloss.JoinHorizontal(lipgloss.Center, builtLine, info)
+	return fmt.Sprintf("%s\n%s\n%s", s.margins.header, s.View(), s.margins.footer)
 }
 
 func (s DataScope) Done() bool {
 	return s.done
 }
 
-func CobraNew(data []string) (p *tea.Program) {
-	return tea.NewProgram(NewDataScope(data))
+func CobraNew(data []string, title string) (p *tea.Program) {
+	return tea.NewProgram(NewDataScope(data, false, title))
 }
 
 //#endregion For Cobra Usage
 
-func NewDataScope(data []string) DataScope {
+func NewDataScope(data []string, motherRunning bool, title string) DataScope {
 	// set up backend paginator
 	p := paginator.New()
 	p.Type = paginator.Dots
@@ -79,7 +134,21 @@ func NewDataScope(data []string) DataScope {
 	p.InactiveDot = lipgloss.NewStyle().Foreground(lipgloss.AdaptiveColor{Light: "250", Dark: "238"}).Render("•")
 	p.SetTotalPages(len(data))
 
-	return DataScope{pager: p}
+	return DataScope{
+		pager:         p,
+		ready:         false,
+		data:          data,
+		done:          false,
+		Title:         title,
+		motherRunning: motherRunning,
+	}
+}
+
+// displays the current page
+func (s DataScope) displayPage() string {
+	start, end := s.pager.GetSliceBounds(len(s.data))
+	imploded := strings.Join(s.data[start:end], "\n")
+	return imploded
 }
 
 // #region styling
