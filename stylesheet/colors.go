99b072438df7b2a1fa12b1ada74c55a7d46a6b9a
R Landau <rflandau@pm.me>
2024-06-24 12:53:15 -0400

    modify color scheme

--- stylesheet/colors.go
+++ stylesheet/colors.go
@@ -2,15 +2,24 @@ package stylesheet
 
 import "github.com/charmbracelet/lipgloss"
 
+// The Primary+Accents are based on a triadic scheme with #9c7af7 at the head
+// The secondary, tertiary colors are analogous to the primary
+
 const (
-	PrimaryColor = lipgloss.Color("#7aa2f7")
-	ErrorColor   = lipgloss.Color("#B22")
-	NavColor     = lipgloss.Color("#AAAAFF")
-	ActionColor  = lipgloss.Color("#FFAAAA")
+	PrimaryColor   = lipgloss.Color("#9c7af7")
+	SecondaryColor = lipgloss.Color("#bb7af7")
+	TertiaryColor  = lipgloss.Color("#f77af4")
+	AccentColor1   = lipgloss.Color("#f79c7a")
+	AccentColor2   = lipgloss.Color("#7af79c")
+	ErrorColor     = lipgloss.Color("#f77a96")
+	NavColor       = SecondaryColor
+	ActionColor    = AccentColor1
+	FocusedColor   = PrimaryColor   // an element currently in focus
+	UnfocusedColor = SecondaryColor // complimentary elements to the focused element
 )
 
 const ( // table colors
-	borderColor = lipgloss.Color("#bb9af7")
-	row1Color   = lipgloss.Color("#c0caf5")
-	row2Color   = lipgloss.Color("#9aa5ce")
+	borderColor = PrimaryColor
+	row1Color   = SecondaryColor
+	row2Color   = TertiaryColor
 )
