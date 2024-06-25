a805ed266f16ad365707a52ca2858f97a22c25de
R Landau <rflandau@pm.me>
2024-06-25 13:45:16 -0400

    sequence setArg cmds, but allow history to be pushed independently

--- mother/mother.go
+++ mother/mother.go
@@ -321,7 +321,7 @@ func processInput(m *Mother) tea.Cmd {
 		clilog.Writer.Errorf("walking input %v returned invalid", given)
 	}
 
-	return tea.Batch(onComplete...)
+	return tea.Batch(onComplete[0], tea.Sequence(onComplete[1:]...))
 }
 
 // pushToHistory generates and stores historical record of the prompt (as a
