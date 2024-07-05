ab7ef12179834299846a8ee505e56b539fc7af19
R Landau <rflandau@pm.me>
2024-07-05 09:32:11 -0400

    update text

--- stylesheet/flagtext.go
+++ stylesheet/flagtext.go
@@ -6,7 +6,7 @@ package stylesheet
 
 const (
 	FlagOutputDesc = "file to write results to. Truncates file unless --append is also given."
-	FlagAppendDesc = "append to the given output file instead of truncating."
+	FlagAppendDesc = "append to the given output file instead of truncating it."
 	FlagCSVDesc    = "display results as CSV. Only effectual with --output. Mutually exclusive with JSON."
 	FlagJSONDesc   = "display results as JSON. Only effectual with --output. Mutually exclusive with CSV."
 )
