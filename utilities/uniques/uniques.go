afd4442aa6221b652d4646c79da2d3cfb1910227
R Landau <rflandau@pm.me>
2024-07-11 11:23:07 -0400

    create global constants package

---
+++ utilities/uniques/uniques.go
@@ -0,0 +1,7 @@
+// uniques contains global constants that must be referenced across multiple packages
+package uniques
+
+const (
+	// the string format the Gravwell client requires
+	SearchTimeFormat = "2006-01-02T15:04:05.999999999Z07:00"
+)
