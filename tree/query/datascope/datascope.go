d700fbe15bb9c8afc8ad3e5f224f20b6514b506b
R Landau <rflandau@pm.me>
2024-07-12 12:25:49 -0400

    implement heartbeat for the current search

--- tree/query/datascope/datascope.go
+++ tree/query/datascope/datascope.go
@@ -1,9 +1,15 @@
 // Datascope is tabbed, scrolling viewport with a paginator built into the results view.
-// It displays arbitrary data, one page at a time, in the alt buffer.
+// It displays and manages results from a search.
 // As the user pages through, the viewport automatically updates with the contents of the new page.
-// The first tab contains the actual results, while
+// The first tab contains the actual results, while the following tabs provide controls for
+// downloading the results and scheduling the query
 //
-// Like busywait, this can be invoked for Cobra or for Mother.
+// Like busywait, this can be invoked by Cobra as a standalone tea.Model or as a child of an action
+// spawned by Mother.
+//
+// ! There can only be one instance of Datascope running within a giving program; you should not
+// compose datascopes from multiple searches. This is a caveat of the self-destructive goroutine
+// used to keep the search object from aging out on the Gravwell backend.
 package datascope
 
 import (
@@ -11,10 +17,12 @@ import (
 	"fmt"
 	"gwcli/clilog"
 	"gwcli/stylesheet"
+	activesearchlock "gwcli/tree/query/datascope/ActiveSearchLock"
 	"gwcli/utilities/killer"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/paginator"
@@ -24,6 +32,79 @@ import (
 	"github.com/charmbracelet/x/term"
 	grav "github.com/gravwell/gravwell/v3/client"
 )
+
+//#region keepAlive syncronization
+
+const (
+	pingFrequency = 50 * time.Second
+	ageOut        = 5 * 60 * time.Second // 5 minutes
+)
+
+// Meant to be called as a goroutine that provides a heartbeat for the search id.
+// This goroutine is self-destructive; if either:
+//
+//   - DS.Update() was last called too long ago (dictated by ageOut)
+//
+//     or
+//
+//   - the search ID this goroutine is tracking is replaced
+//
+// the goroutine will die on next cycle.
+func keepAlive(search *grav.Search) {
+	/**
+	 * A long-winded explanation of the problem this solves:
+	 * This self-destructive goroutine-as-a-heartbeat is a necessary complilation over just having
+	 * a gorutine that sleeps->pings->sleeps->(repeat ad nauseam) for the simple fact that DataScope
+	 * (or any child or grandchild of Mother) does not know of its own death. When invoking actions
+	 * non-interactively, we do not have to worry about this; goroutines die with their progenitor.
+	 * However, a TUI is a single, long-running process; we must clean up after ourselves.
+	 *
+	 * More to the point, Mother is designed to always be able to re-assert control over her
+	 * children, hence why she handles kill keys. The byproduct of this, however, is that we have no
+	 * guarantee that a child action will be able to gracefully exit. Therefore, we need a mechanism
+	 * to reap these goroutines (they aren't technically zombies in the same way a process would be,
+	 * but the concept is the same).
+	 *
+	 * Mother could reap keepAlive, but 1) Mother is designed to be agnostic and does not care what
+	 * her children are doing or if they have invoked DS and 2) we would need an event bus of sorts
+	 * for mother to even have access to the context or channel with which to kill keepAlive.
+	 *
+	 * DS.Update() could ping for the search, but then frequency modulation becomes a problem.
+	 * We can set a ticker so only the Update closest to the expiration of the ticker pings, but
+	 * this adds weight to Update and only serves to limits pings; we have no way of guaranteeing
+	 * that Update will be called frequently /enough/ for keepAlive.
+	 *
+	 * Thus, the best option is this: a goroutine that kills itself if it has reason to believe
+	 * that its instance of DS is dead.
+	 * Note that this solution still suffers from potentially too-infrequent Update calls. However,
+	 * I believe this scenario is uncommon enough to be a risk worth taking; between the user
+	 * navigating DS and the textinputs sending back blinks, we should be okay in most use cases.
+	 *
+	 */
+	var mysid = search.ID
+	for {
+		if cursid := activesearchlock.GetSearchID(); cursid != mysid { // search ID changed
+			clilog.Writer.Debugf("keepAlive: sid changed from %v to %v. Dying...", mysid, cursid)
+			break
+		}
+		lastTS := activesearchlock.GetTS()
+		oldestViableTS := time.Now().Unix() - int64(ageOut.Seconds())
+		if oldestViableTS > lastTS { // last update was too long ago
+			clilog.Writer.Debugf("keepAlive: search aged out (oldest viable %v > last %v). Dying...",
+				oldestViableTS, lastTS)
+			break
+		}
+
+		if err := search.Ping(); err != nil {
+			clilog.Writer.Warnf("keepAlive: ping failed: %v", err)
+			break
+		}
+		clilog.Writer.Debugf("pinged search %v", mysid)
+		time.Sleep(pingFrequency)
+	}
+}
+
+//#endregion
 
 type DataScope struct {
 	vp            viewport.Model
@@ -96,6 +177,12 @@ func NewDataScope(data []string, motherRunning bool, search *grav.Search, outfn 
 		}
 	}
 
+	// store data for keepAlive
+	activesearchlock.SetSearchID(search.ID)
+	activesearchlock.UpdateTS()
+	// launch heartbeat gorotuine
+	go keepAlive(search)
+
 	// mother does not start in alt screen, and thus requires manual measurements
 	if motherRunning {
 		return s, tea.Sequence(tea.EnterAltScreen, func() tea.Msg {
@@ -115,8 +202,6 @@ func (s DataScope) Init() tea.Cmd {
 }
 
 func (s DataScope) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	//clilog.Writer.Debugf("msg: %v\ntab: %v", msg, s.tabs[s.activeTab].name)
-
 	// mother takes care of kill keys if she is running
 	if !s.motherRunning {
 		if kill := killer.CheckKillKeys(msg); kill != killer.None {
@@ -124,6 +209,9 @@ func (s DataScope) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			return s, tea.Batch(tea.Quit, tea.ExitAltScreen)
 		}
 	}
+
+	// update the timestamp to keep the heartbeat going
+	activesearchlock.UpdateTS()
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg: // tab-agnostic keys
