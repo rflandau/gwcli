95cbb74a7f6f3ae9ef46d23de7c2e2a749797078
R Landau <rflandau@pm.me>
2024-07-12 12:27:17 -0400

    annotate activesearchlock

--- tree/query/datascope/ActiveSearchLock/activesearchlock.go
+++ tree/query/datascope/ActiveSearchLock/activesearchlock.go
@@ -1,3 +1,5 @@
+// activesearchlock is a simple package for controlling the singleton locks DS's keepalive
+// uses to check for its own viability.
 package activesearchlock
 
 import (
@@ -12,27 +14,28 @@ var ( // the shared variables protected by this package
 	ts     int64
 )
 
-// Atomically gets sid
+// Atomically gets sid.
 func GetSearchID() string {
 	sIDMtx.Lock()
 	defer sIDMtx.Unlock()
 	return sID
 }
 
-// Atomically sets sid
+// Atomically sets sid.
 func SetSearchID(sid string) {
 	sIDMtx.Lock()
 	sID = sid
 	sIDMtx.Unlock()
 }
 
-// Atomically updates the timestamp to the current unix time
+// Atomically updates the timestamp to the current unix time.
 func UpdateTS() {
 	tsMtx.Lock()
 	ts = time.Now().Unix()
 	tsMtx.Unlock()
 }
 
+// Atomically gets the last timestamp.
 func GetTS() int64 {
 	tsMtx.Lock()
 	defer tsMtx.Unlock()
