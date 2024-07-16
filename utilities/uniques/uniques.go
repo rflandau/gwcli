f60564b89dfd00875a5c67bd0d242a5df604e376
R Landau <rflandau@pm.me>
2024-07-16 10:53:10 -0400

    do not warn check in uniques

--- utilities/uniques/uniques.go
+++ utilities/uniques/uniques.go
@@ -3,7 +3,6 @@ package uniques
 
 import (
 	"fmt"
-	"gwcli/clilog"
 )
 
 const (
@@ -11,11 +10,7 @@ const (
 	SearchTimeFormat = "2006-01-02T15:04:05.999999999Z07:00"
 )
 
-// Returns a *newline-suffixed* string about ignoring a flag due to causeFlag's existance (or the
-// empty string if WARN would not be printed).
+// Returns a string about ignoring a flag due to causeFlag's existance
 func WarnFlagIgnore(ignoredFlag, causeFlag string) string {
-	if clilog.Active(clilog.WARN) {
-		return fmt.Sprintf("WARN: ignoring flag --%v due to --%v\n", ignoredFlag, causeFlag)
-	}
-	return ""
+	return fmt.Sprintf("WARN: ignoring flag --%v due to --%v", ignoredFlag, causeFlag)
 }
