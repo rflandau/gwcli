9ef2c7d251a45f5a145e66f21a878b4398c43154
R Landau <rflandau@pm.me>
2024-07-17 16:22:53 -0400

    consistently style the index numbers

--- tree/query/datascope/tabletab.go
+++ tree/query/datascope/tabletab.go
@@ -53,7 +53,7 @@ func initTableTab(data []string) tableTab {
 		// map each row cell to its column
 		rd := table.RowData{}
 		// prepend the index column
-		rd["index"] = i + 1
+		rd["index"] = indexStyle.Render(strconv.Itoa(i + 1))
 		for j, c := range cells {
 			rd[strconv.Itoa(j+1)] = c
 		}
