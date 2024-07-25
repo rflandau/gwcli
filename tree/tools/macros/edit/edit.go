17914b43487dc4d0653fbde1b2c500e0a3cac76d
R Landau <rflandau@pm.me>
2024-07-25 17:20:56 -0400

    WIP edit scaffold as macro edit

--- tree/tools/macros/edit/edit.go
+++ tree/tools/macros/edit/edit.go
@@ -10,6 +10,7 @@ import (
 	"gwcli/stylesheet/colorizer"
 	"gwcli/utilities/treeutils"
 	"gwcli/utilities/uniques"
+	"slices"
 	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
@@ -39,8 +40,11 @@ const listHeightMax = 40 // lines
 
 type getFunction = func(id uint64) (item types.SearchMacro, err error)
 type fetchFunction = func() ([]types.SearchMacro, error)
+type translateFunction = func(item types.SearchMacro, fieldKey string) (string, error)
 
 //#endregion
+
+type Config = map[string]Field
 
 // #region local styles
 var (
@@ -60,6 +64,14 @@ func NewMacroEditAction() action.Pair {
 	// fetch available macros in SetArgs or run, in case macros are updated prior to usage
 	// TODO run fetch in run() if an explicit ID is not given
 
+	// TODO temporary
+	cfg := Config{
+		"name": Field{
+			Required: true,
+			Title:    "Name",
+			Usage:    stylesheet.FlagDescMacroName,
+			FlagName: uniques.DeriveFlagName("Name")},
+	}
 	fchFunc := func() ([]types.SearchMacro, error) {
 		return connection.Client.GetUserMacros(connection.MyInfo.UID)
 	}
@@ -69,7 +81,12 @@ func NewMacroEditAction() action.Pair {
 	cmd.Flags().AddFlagSet(&flags)
 	cmd.Flags().AddFlagSet(&aflags)
 
-	return treeutils.GenerateAction(cmd, newEditModel(fchFunc, connection.Client.GetMacro, addtlFlags))
+	return treeutils.GenerateAction(cmd, newEditModel(
+		cfg,
+		fchFunc,
+		connection.Client.GetMacro,
+		macroTranslation,
+		addtlFlags))
 }
 
 // base flagset always available to edit actions
@@ -88,6 +105,20 @@ func addtlFlags() pflag.FlagSet {
 	return fs
 }
 
+// translate fields keys into their value in the given item
+func macroTranslation(item types.SearchMacro, fieldKey string) (string, error) {
+	switch fieldKey {
+	case "name":
+		return item.Name, nil
+	case "description":
+		return item.Description, nil
+	case "expansion":
+		return item.Expansion, nil
+	}
+
+	return "", fmt.Errorf("unknown field key: %v", fieldKey)
+}
+
 //#region interactive mode (model) implementation
 
 type mode = uint8
@@ -99,10 +130,9 @@ const (
 	idle                  // inactive
 )
 
-type titledTI struct {
-	title    string          // field name to display next to the TI
-	ti       textinput.Model // ti for user modifications
-	required bool            // this field must not be empty
+type keyedTI struct {
+	key string          // key to look up the related field in the Config
+	ti  textinput.Model // ti for user modifications
 }
 
 type editModel struct {
@@ -111,15 +141,18 @@ type editModel struct {
 	fs            pflag.FlagSet        // current state of the flagset
 	width, height int
 
-	data    []types.SearchMacro // data retrieved by fchFunc
-	getFunc getFunction         // func to retrieve a specified editable item
+	cfg Config // RO configuration provided by the caller
+
+	data      []types.SearchMacro // data retrieved by fchFunc
+	getFunc   getFunction         // func to retrieve a specified editable item
+	transFunc translateFunction   // func to retrieve field values from a struct
 
 	// selecting mode
 	fchFunc fetchFunction // func to retrieve each editable item
 	list    list.Model    // list displayed during `selecting` mode
 
 	// editting mode
-	ttis         []titledTI        // TIs will be displayed in array order
+	orderedKTIs  []keyedTI         // TIs will be displayed in array order
 	tiIndex      int               // array index of active TI
 	tiCount      int               // len(ttis)
 	selectedData types.SearchMacro // item chosen from the list
@@ -134,8 +167,15 @@ type editModel struct {
 // getFunc must be the function for getting a single struct, given id.
 //
 // addtlFlagFunc may be nil or a function that returns a new flagset to parse/extract values from.
-func newEditModel(fchFunc fetchFunction, getFunc getFunction, addtlFlagFunc func() pflag.FlagSet) *editModel {
+func newEditModel(config Config,
+	fchFunc fetchFunction,
+	getFunc getFunction,
+	transFunc translateFunction,
+	addtlFlagFunc func() pflag.FlagSet) *editModel {
 	// sanity check required arguments
+	if config == nil {
+		panic("Configuration cannot be nil")
+	}
 	if fchFunc == nil {
 		panic("fetch function cannot be nil")
 	}
@@ -144,7 +184,15 @@ func newEditModel(fchFunc fetchFunction, getFunc getFunction, addtlFlagFunc func
 		panic("get function cannot be nil")
 	}
 
-	em := &editModel{mode: idle, fchFunc: fchFunc, addtlFlagFunc: addtlFlagFunc, getFunc: getFunc}
+	if transFunc == nil {
+		panic("translation function cannot be nil")
+	}
+
+	em := &editModel{mode: idle,
+		cfg:           config,
+		fchFunc:       fchFunc,
+		addtlFlagFunc: addtlFlagFunc,
+		getFunc:       getFunc}
 	if em.addtlFlagFunc != nil {
 		em.fs = em.addtlFlagFunc()
 	} else {
@@ -190,15 +238,15 @@ func (em *editModel) Update(msg tea.Msg) tea.Cmd {
 			case tea.KeyEnter:
 				if keymsg.Alt {
 					var populated bool = true
-					for _, tti := range em.ttis { // check all required fields are populated
-						if tti.required && strings.TrimSpace(tti.ti.Value()) == "" {
-							em.inputErr = tti.title + " is required"
+					for _, kti := range em.orderedKTIs { // check all required fields are populated
+						if em.cfg[kti.key].Required && strings.TrimSpace(kti.ti.Value()) == "" {
+							em.inputErr = kti.key + " is required"
 							populated = false
 							break
 						}
 					}
 					if populated {
-						if invalMsg, err := upd(em.ttis, em.selectedData); err != nil {
+						if invalMsg, err := upd(em.orderedKTIs, em.selectedData); err != nil {
 							em.updateErr = err.Error()
 						} else if invalMsg != "" {
 							em.inputErr = invalMsg
@@ -221,9 +269,9 @@ func (em *editModel) Update(msg tea.Msg) tea.Cmd {
 		}
 
 		// update tis
-		cmds := make([]tea.Cmd, len(em.ttis))
-		for i, tti := range em.ttis {
-			em.ttis[i].ti, cmds[i] = tti.ti.Update(msg)
+		cmds := make([]tea.Cmd, len(em.orderedKTIs))
+		for i, tti := range em.orderedKTIs {
+			em.orderedKTIs[i].ti, cmds[i] = tti.ti.Update(msg)
 		}
 		cmd = tea.Batch(cmds...)
 	}
@@ -233,77 +281,97 @@ func (em *editModel) Update(msg tea.Msg) tea.Cmd {
 
 // Blur existing TI, select and focus previous (higher) TI
 func (em *editModel) previousTI() {
-	em.ttis[em.tiIndex].ti.Blur()
+	em.orderedKTIs[em.tiIndex].ti.Blur()
 	em.tiIndex -= 1
 	if em.tiIndex < 0 {
 		em.tiIndex = em.tiCount - 1
 	}
-	em.ttis[em.tiIndex].ti.Focus()
+	em.orderedKTIs[em.tiIndex].ti.Focus()
 }
 
 // Blur existing TI, select and focus next (lower) TI
 func (em *editModel) nextTI() {
-	em.ttis[em.tiIndex].ti.Blur()
+	em.orderedKTIs[em.tiIndex].ti.Blur()
 	em.tiIndex += 1
 	if em.tiIndex >= em.tiCount {
 		em.tiIndex = 0
 	}
-	em.ttis[em.tiIndex].ti.Focus()
+	em.orderedKTIs[em.tiIndex].ti.Focus()
 }
 
-// Takes the struct associated to the selected list item and transform its edit-able fields into a
-// list of TIs.
-func transmuteStruct(data types.SearchMacro, fs pflag.FlagSet) []titledTI {
-	var tis []titledTI = make([]titledTI, 3)
+type Field struct {
+	Required      bool   // is this field required to be populated?
+	Title         string // field name displayed next to prompt and as flage name
+	Usage         string // OPTIONAL. Flag usage displayed via -h
+	FlagName      string // OPTIONAL. Defaults to DeriveFlagName() result.
+	FlagShorthand rune   // OPTIONAL. '-x' form of FlagName.
+	Order         int    // OPTIONAL. Top-Down (highest to lowest) display order of this field.
 
-	tis[0] = titledTI{ // name
-		title:    "Name",
-		ti:       stylesheet.NewTI(data.Name, false),
-		required: true,
+	// OPTIONAL.
+	// Called once, at program start to generate a TI instead of using a generalize newTI()
+	CustomTIFuncInit func() textinput.Model
+}
+
+// Transmute generates the list of TIs using the provided Field configuration. Fields with changed
+// flags have their default set to the flags values. Fields without are given to the implementor to
+// manually populate their default values from data's fields (see translate()).
+func transmuteStruct(data types.SearchMacro,
+	fs pflag.FlagSet,
+	cfg Config,
+	translateFunc translateFunction) (
+	[]keyedTI, error,
+) {
+	var orderedKTIs []keyedTI = make([]keyedTI, len(cfg))
+
+	var i uint8 = 0
+
+	for k, v := range cfg {
+		// create the TI, using the custom creation function if defined
+		var ti textinput.Model
+		if v.CustomTIFuncInit != nil {
+			ti = v.CustomTIFuncInit()
+		} else {
+			ti = stylesheet.NewTI("", v.Required)
+		}
+
+		if fs.Changed(v.FlagName) { // if this field's flag was changed, update its default value
+			// fetch the value
+			if x, err := fs.GetString(v.FlagName); err != nil {
+				return nil, err
+			} else {
+				ti.SetValue(x)
+			}
+		} else {
+			// if this flag was not set,
+			// the implementor must map it to the corresponding struct field
+			clilog.Writer.Debugf("field %v requires translation", k)
+			if t, err := translateFunc(data, k); err != nil {
+				return nil, err
+			} else {
+				ti.SetValue(t)
+			}
+		}
+
+		// add the TI to the list
+		i += 1
+		orderedKTIs[i] = keyedTI{key: k, ti: ti}
 	}
 
-	// check for name flag
-	if x, err := fs.GetString(stylesheet.FlagNameMacroName); err != nil {
-		clilog.LogFlagFailedGet(stylesheet.FlagNameMacroName, err)
-	} else if fs.Changed(stylesheet.FlagNameMacroName) {
-		tis[0].ti.SetValue(x)
-	}
+	// with TIs built, sort them by order
+	slices.SortFunc(orderedKTIs, func(a, b keyedTI) int {
+		return cfg[b.key].Order - cfg[a.key].Order
+	})
 
-	tis[1] = titledTI{ // description
-		title:    "Description",
-		ti:       stylesheet.NewTI(data.Description, false),
-		required: true,
-	}
-
-	// check for description flag
-	if x, err := fs.GetString("description"); err != nil {
-		clilog.LogFlagFailedGet("description", err)
-	} else if fs.Changed("description") {
-		tis[1].ti.SetValue(x)
-	}
-
-	tis[2] = titledTI{ // description
-		title:    "Expansion",
-		ti:       stylesheet.NewTI(data.Expansion, false),
-		required: true,
-	}
-	// check for description flag
-	if x, err := fs.GetString(stylesheet.FlagNameMacroExpansion); err != nil {
-		clilog.LogFlagFailedGet(stylesheet.FlagNameMacroExpansion, err)
-	} else if fs.Changed(stylesheet.FlagNameMacroExpansion) {
-		tis[2].ti.SetValue(x)
-	}
-
-	return tis
+	return orderedKTIs, nil
 }
 
 // Takes the populated TIs, validates their input, and updates the gravwell backend.
-func upd(ttis []titledTI, data types.SearchMacro) (invalMsg string, err error) {
+func upd(ttis []keyedTI, data types.SearchMacro) (invalMsg string, err error) {
 	// no need to nil check; all required fields are checked already
 
 	// rebuild the struct for the update call
 	for i, tti := range ttis {
-		switch tti.title {
+		switch tti.key {
 		case "Name":
 			data.Name = strings.ToUpper(tti.ti.Value()) // name must always be uppercase
 			ttis[i].ti.SetValue(data.Name)              // update it in case we return invalid or err
@@ -333,14 +401,14 @@ func (em *editModel) View() string {
 				Render("Press enter to select")
 	case editting:
 		var sb strings.Builder
-		for _, tti := range em.ttis {
+		for _, kti := range em.orderedKTIs {
 			// color the title appropriately
-			if tti.required {
-				sb.WriteString(tiFieldRequiredSty.Render(tti.title + ": "))
+			if em.cfg[kti.key].Required {
+				sb.WriteString(tiFieldRequiredSty.Render(kti.key + ": "))
 			} else {
-				sb.WriteString(tiFieldOptionalSty.Render(tti.title + ": "))
+				sb.WriteString(tiFieldOptionalSty.Render(kti.key + ": "))
 			}
-			sb.WriteString(tti.ti.View() + "\n")
+			sb.WriteString(kti.ti.View() + "\n")
 		}
 		sb.WriteString(colorizer.SubmitString("alt+enter", em.inputErr, "", em.width))
 		str = sb.String()
@@ -365,7 +433,7 @@ func (em *editModel) Reset() error {
 	em.list = list.Model{}
 
 	// editting mode
-	em.ttis = nil
+	em.orderedKTIs = nil
 	em.tiIndex = 0
 	em.tiCount = 0
 	em.selectedData = types.SearchMacro{}
@@ -437,13 +505,16 @@ func (em *editModel) enterEditMode() error {
 	clilog.Writer.Debugf("editting macro %v", em.selectedData.Name)
 
 	// transmute the selected item into a series of TIs
-	em.ttis = transmuteStruct(em.selectedData, em.fs)
-	em.tiCount = len(em.ttis)
+	var err error
+	if em.orderedKTIs, err = transmuteStruct(em.selectedData, em.fs); err != nil {
+		return err
+	}
+	em.tiCount = len(em.orderedKTIs)
 	if em.tiCount < 1 {
 		return errors.New("no TIs created by transmutation")
 	}
 
-	em.ttis[0].ti.Focus() // focus the first TI
+	em.orderedKTIs[0].ti.Focus() // focus the first TI
 
 	em.mode = editting
 	return nil
